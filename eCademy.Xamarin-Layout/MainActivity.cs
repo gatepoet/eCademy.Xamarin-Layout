6b649cfd3c00918b8836b56870cf3a0af145c118
kristoffer.deinoff <kristoffer.deinoff@gmail.com>
2018-01-03 17:37:13 +0100

    Add project files.

---
+++ eCademy.Xamarin-Layout/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace eCademy.Xamarin_Layout
+{
+    [Activity(Label = "eCademy.Xamarin_Layout", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
